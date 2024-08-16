914b83fc96ad9ad104a366a0f663a561441f7cdc
gigikale <luckygloria93@gmail.com>
2024-08-16 15:13:08 -0500

    Footer page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
-import Hero from './components/Hero';
-import About from './components/About';
-import backgroundImage from './assets/glowbg.png';
-import Video from './components/Video';
-import Ecosystem from './components/Ecosystem';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './home/LandingPage'; 
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
-      <div
-        className="relative bg-cover bg-no-repeat min-h-screen pb-32"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-90"></div>
-
-        <div className="relative z-10">
-          <Hero />
-          <About />
-        </div>
+    <Router>
+      <div>
+        <p className="text-4xl font-bold text-blue-500">Theodora</p>
+        <Routes>
+          <Route path="/" element={<LandingPage />} /> 
+        </Routes>
       </div>
-      <div
-        className="relative bg-cover bg-no-repeat min-h-screen"
-        style={{ backgroundImage: `url(${backgroundImage})`,   backgroundPosition: 'left center' }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-90"></div>
-
-        <div className="relative z-10">
-          <Video />
-          <Ecosystem />
-        </div>
-      </div>
-    </div>
-
+    </Router>
   );
 }
 
